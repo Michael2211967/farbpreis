8633b774865235d2283a77fa9c2a353b0f8b9641
Michael Laubach <MLaubach@t-online.de>
2025-02-07 14:41:44 +0100

    added sub textupper

--- Windows/farbpreis.c
+++ Windows/farbpreis.c
@@ -12,37 +12,32 @@ float area;
 float quantity;
 float price;
 char answer[10];
-char text[80];
 int i;
 
-char textupper(char text[])
-{
-	for (i=0; i<=strlen(text); i++)
-		text[i] = toupper(text[i]);
-	return text;
-}
-
 int main()
-{	
+{
 	printf("\nPreis je Kilo Farbe eingeben: ");
 	scanf("%f", &kiloprice);
 	printf("Kilo je Quadratmeter eingeben: ");
 	scanf("%f", &kiloquantity);
 	do
 	{
-		printf("\nLänge eingeben: ");
+		printf("\nLaenge eingeben: ");
 		scanf("%f", &length);
 		printf("Breite eingeben: ");
 		scanf("%f", &width);
 		area = length * width;
 		quantity = area * kiloquantity;
 		price = quantity * kiloprice;
-		printf("\nFür die Fläche von %.2f qm\n", area);
-		printf("werden %.2f Kilo Farbe benötigt.\n", quantity);
-		printf("Der Preis für diese %.2f kg Farbe beträgt %.2f EUR.\n", quantity, price);
-		printf("\nWollen Sie noch eine neue Fläche berechnen? ");
+		printf("\nFuer die Flaeche von %.2f qm\n", area);
+		printf("werden %.2f Kilo Farbe benoetigt.\n", quantity);
+		printf("Der Preis fuer diese %.2f kg Farbe betraegt %.2f EUR.\n", quantity, price);
+		printf("\nWollen Sie noch eine neue Flaeche berechnen? ");
 		scanf("%s", answer);
-		textupper(answer);
+		for (i=0; i<=strlen(answer); i++)
+		{
+			answer[i] = toupper(answer[i]);
+		}
 	} while (strcmp(answer, "JA") == 0);
 	puts("\nAuf Wiedersehen\n");
 }
