7bc3a4cb4479445127481281913cc124169152d8
Michael2211967 <106823044+Michael2211967@users.noreply.github.com>
2026-01-16 22:14:02 +0100

    excluded textupper to textupper.h and textupper.c

--- farbpreis.c
+++ farbpreis.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include "textupper.h"
 
 float kiloprice;
 float kiloquantity;
@@ -12,15 +13,6 @@ float area;
 float quantity;
 float price;
 char answer[10];
-char text[80];
-int i;
-
-char* textupper(char text[])
-{
-	for (i=0; i<=strlen(text); i++)
-		text[i] = toupper(text[i]);
-	return text;
-}
 
 int main()
 {	
@@ -41,7 +33,7 @@ int main()
 		printf("werden %.2f Kilo Farbe benötigt.\n", quantity);
 		printf("Der Preis für diese %.2f kg Farbe beträgt %.2f EUR.\n", quantity, price);
 		printf("\nWollen Sie noch eine neue Fläche berechnen? ");
-		scanf("%s", answer);
+		scanf("%10s", answer);
 		textupper(answer);
 	} while (strcmp(answer, "JA") == 0);
 	puts("\nAuf Wiedersehen\n");
